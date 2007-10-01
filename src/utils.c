ashgti/parrot

commit 8f8b65a33b8b098050a1c7606bafaec376504a5c

[src] Optimized Parrot_byte_index(), which was surprisingly expensive.  It's a little bit tricky because C strings can't have embedded NULL characters, so the naive use of the standard library's index() doesn't always work.  However, it's a good place to start as it tends to run much faster than the inchworm code here.
Another optimization is never paying the price of calling memcmp() if it's
obvious that it will fail.

There are larger algorithmic optimizations to make calling this code less
expensive (by calling it less frequently), but this was a low-hanging
optimization.  Amortized over the data of PGE's regexp tests, this function
runs about three times faster than the previous version.

git-svn-id: https://svn.parrot.org/parrot/trunk@21669 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

src/utils.c
@@ -647,17 +647,32 @@ INTVAL
 Parrot_byte_index(SHIM_INTERP, NOTNULL(const STRING *base),
         NOTNULL(const STRING *search), UINTVAL start_offset)
 {
-    const INTVAL searchlen = search->strlen;
+    const INTVAL       searchlen    = search->strlen;
     const char * const search_start = search->strstart;
-    const INTVAL max_possible_offset = (base->strlen - search->strlen);
-    INTVAL current_offset;
+    const char * const base_start   = base->strstart;
+    const INTVAL       max_offset   = base->strlen - searchlen;
+    const char * const max_pos      = base_start + max_offset;
 
-    for (current_offset = start_offset; current_offset <= max_possible_offset;
-            current_offset++) {
-        const char * const base_start = (char *)base->strstart + current_offset;
-        if (memcmp(base_start, search_start, searchlen) == 0) {
-            return current_offset;
-        }
+    /* looking for the first character of the search string can avoid having to
+     * inch along, comparing everything.  However, if the base string has
+     * embedded nulls, index() will stop at the first one, so inching along is
+     * the only good solution */
+    char *start_pos = index(base_start + start_offset, *search_start);
+
+    /* index() returns NULL for no match, so start at the start position */
+    if (!start_pos)
+        start_pos = base_start + start_offset;
+
+    while (start_pos <= max_pos) {
+        /* only do the memcmp() if the first character matches */
+        if (  *start_pos == *search_start
+            && memcmp(start_pos, search_start, searchlen) == 0)
+
+            /* return the offset, not the pointer to the position */
+            return start_pos - base_start;
+
+        /* walking is necessary in case of embedded null bytes */
+        start_pos++;
     }
 
     return -1;

commit 286db9eb827b289ff19621e2fe9f154af082d8de

[build] on win32 - linking is broken since r21669 : src\utils.o: In function `Parrot_byte_index':C:/fperrad/Parrot/trunk/src/utils.c:660: undefined reference to `index' - replace index() by strchr() which is C89
git-svn-id: https://svn.parrot.org/parrot/trunk@21715 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

src/utils.c
@@ -655,11 +655,11 @@ Parrot_byte_index(SHIM_INTERP, NOTNULL(const STRING *base),
 
     /* looking for the first character of the search string can avoid having to
      * inch along, comparing everything.  However, if the base string has
-     * embedded nulls, index() will stop at the first one, so inching along is
+     * embedded nulls, strchr() will stop at the first one, so inching along is
      * the only good solution */
-    char *start_pos = index(base_start + start_offset, *search_start);
+    char *start_pos = strchr(base_start + start_offset, *search_start);
 
-    /* index() returns NULL for no match, so start at the start position */
+    /* strchr() returns NULL for no match, so start at the start position */
     if (!start_pos)
         start_pos = base_start + start_offset;
 