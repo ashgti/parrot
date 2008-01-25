ashgti/parrot

commit 30b55db30ca11eb6ab4011e4c22c4a19255cd39e

Added missing break to switch statement (!!!).  Added checks for NULL pointers
git-svn-id: https://svn.parrot.org/parrot/trunk@25208 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

src/exceptions.c
@@ -600,6 +600,8 @@ rethrow_exception(PARROT_INTERP, ARGIN(PMC *exception))
     if (exception->vtable->base_type != enum_class_Exception)
         PANIC(interp, "Illegal rethrow");
     handler = find_exception_handler(interp, exception);
+    if (!handler)
+        PANIC(interp, "No exception handler found");
     address = VTABLE_invoke(interp, handler, exception);
     /* return the address of the handler */
     return address;
@@ -664,8 +666,10 @@ dest2offset(PARROT_INTERP, ARGIN(const opcode_t *dest))
         case PARROT_CGP_CORE:
         case PARROT_CGP_JIT_CORE:
             offset = dest - (const opcode_t *)interp->code->prederef.code;
+            break;
         default:
             offset = dest - interp->code->base.data;
+            break;
     }
     return offset;
 }
@@ -735,8 +739,8 @@ handle_exception(PARROT_INTERP)
     /* absolute address of handler */
     const opcode_t * const dest = create_exception(interp);
 
-    /* XXX We don't know that dest will be non-NULL, and it's not legal */
-    /* to pass NULL to dest2offset. */
+    if (!dest)
+        PANIC(interp,"Unable to create exception");
 
     return dest2offset(interp, dest);
 }

commit 67111c362fd4c75744e7aea0420d52ccf32637f4

[src] Fix runcores accidentally broken in r25208.
To keep this from coming up again (second time I've reverted a mistaken break
here), I removed dest2offset() altogether, as it only gets used from one place
and becomes a one-liner anyway.

git-svn-id: https://svn.parrot.org/parrot/trunk@25213 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

src/exceptions.c
@@ -47,11 +47,6 @@ static opcode_t * create_exception(PARROT_INTERP)
         __attribute__nonnull__(1);
 
 PARROT_WARN_UNUSED_RESULT
-static size_t dest2offset(PARROT_INTERP, ARGIN(const opcode_t *dest))
-        __attribute__nonnull__(1)
-        __attribute__nonnull__(2);
-
-PARROT_WARN_UNUSED_RESULT
 PARROT_CAN_RETURN_NULL
 static PMC * find_exception_handler(PARROT_INTERP, ARGIN(PMC *exception))
         __attribute__nonnull__(1)
@@ -643,39 +638,6 @@ rethrow_c_exception(PARROT_INTERP)
 
 /*
 
-=item C<static size_t dest2offset>
-
-Translate an absolute bytecode location to an offset used for resuming
-after an exception had occurred.
-
-=cut
-
-*/
-
-PARROT_WARN_UNUSED_RESULT
-static size_t
-dest2offset(PARROT_INTERP, ARGIN(const opcode_t *dest))
-{
-    size_t offset;
-    /* translate an absolute location in byte_code to an offset
-     * used for resuming after an exception had occurred
-     */
-    switch (interp->run_core) {
-        case PARROT_SWITCH_CORE:
-        case PARROT_SWITCH_JIT_CORE:
-        case PARROT_CGP_CORE:
-        case PARROT_CGP_JIT_CORE:
-            offset = dest - (const opcode_t *)interp->code->prederef.code;
-            break;
-        default:
-            offset = dest - interp->code->base.data;
-            break;
-    }
-    return offset;
-}
-
-/*
-
 =item C<static opcode_t * create_exception>
 
 Create an exception.
@@ -742,7 +704,8 @@ handle_exception(PARROT_INTERP)
     if (!dest)
         PANIC(interp,"Unable to create exception");
 
-    return dest2offset(interp, dest);
+    /* return the *offset* of the handler */
+    return dest - interp->code->base.data;
 }
 
 /*