6450bbf921454a975e6279b756e0f1b26270d580
Whiteknight <Whiteknight@d31e2699-5ff4-0310-a27c-f18f2fbe73fe>
2008-08-19 21:08:02 +0000

    [gsoc_pdd09] update to trunk from r30310
    git-svn-id: https://svn.parrot.org/parrot/branches/gsoc_pdd09@30354 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- src/debug.c
+++ src/debug.c
@@ -406,6 +406,7 @@ debugger_cmdline(PARROT_INTERP)
 
         PDB_run_command(interp, command);
     }
+    TRACEDEB_MSG("debugger_cmdline finished");
 }
 
 static void
@@ -415,6 +416,9 @@ close_script_file(PARROT_INTERP)
     if (interp->pdb->script_file) {
         fclose(interp->pdb->script_file);
         interp->pdb->script_file = NULL;
+        interp->pdb->state|= PDB_STOPPED;
+        interp->pdb->last_command[0] = '\0';
+        interp->pdb->cur_command[0] = '\0';
     }
 }
 
@@ -498,6 +502,8 @@ void
 Parrot_debugger_load(PARROT_INTERP, ARGIN_NULLOK(STRING *filename))
 {
     char *file;
+
+    TRACEDEB_MSG("Parrot_debugger_load");
 
     if (!interp->pdb)
         Parrot_ex_throw_from_c_args(interp, NULL, 0, "No debugger");
@@ -638,14 +644,12 @@ PDB_get_command(PARROT_INTERP)
         FILE *fd = interp->pdb->script_file;
         char buf[DEBUG_CMD_BUFFER_LENGTH+1];
         char *ptr = buf;
-        buf[0]='\0';
 
-        if (feof(fd)) {
-            close_script_file(interp);
-            return;
-        }
         do {
-            fgets(buf, DEBUG_CMD_BUFFER_LENGTH, fd);
+            if (fgets(buf, DEBUG_CMD_BUFFER_LENGTH, fd) == NULL) {
+                close_script_file(interp);
+                return;
+            }
 
             /* skip spaces */
             for (ptr = (char *)&buf; *ptr && isspace((unsigned char)*ptr); ptr++);
@@ -655,7 +659,6 @@ PDB_get_command(PARROT_INTERP)
                 continue;
         } while (0);
 
-        buf[strlen(buf)-1]='\0';
         /* RT #46117: handle command error and print out script line
          *       PDB_run_command should return non-void value?
          *       stop execution of script if fails
@@ -667,7 +670,6 @@ PDB_get_command(PARROT_INTERP)
             close_script_file(interp);
             return;
         }
-        strcpy(pdb->cur_command, buf);
     }
     else {
 
@@ -715,6 +717,8 @@ PDB_script_file(PARROT_INTERP, ARGIN(const char *command))
 {
     FILE *fd;
 
+    TRACEDEB_MSG("PDB_script_file");
+
     /* If already executing a script, close it */
     close_script_file(interp);
 
@@ -728,6 +732,7 @@ PDB_script_file(PARROT_INTERP, ARGIN(const char *command))
         return;
     }
     interp->pdb->script_file = fd;
+    TRACEDEB_MSG("PDB_script_file finished");
 }
 
 /*
@@ -1858,6 +1863,8 @@ PDB_disassemble_op(PARROT_INTERP, ARGOUT(char *dest), int space,
     /* Write the opcode name */
     const char * p = full_name ? info->full_name : info->name;
 
+    TRACEDEB_MSG("PDB_disassemble_op");
+
     if (! p)
         p= "**UNKNOWN**";
     strcpy(dest, p);
@@ -2074,7 +2081,7 @@ PDB_disassemble_op(PARROT_INTERP, ARGOUT(char *dest), int space,
                                      " :unused004",
                                      " :unused008",
                                      " :const",
-                                     " :flat",	/* should be :slurpy for args */
+                                     " :flat", /* should be :slurpy for args */
                                      " :unused040",
                                      " :optional",
                                      " :opt_flag",
@@ -2155,6 +2162,8 @@ PDB_disassemble(PARROT_INTERP, SHIM(const char *command))
     const unsigned int default_size = 32768;
     size_t space;  /* How much space do we have? */
     size_t size, alloced, n;
+
+    TRACEDEB_MSG("PDB_disassemble");
 
     pfile = mem_allocate_typed(PDB_file_t);
     pline = mem_allocate_typed(PDB_line_t);
@@ -2351,8 +2360,11 @@ PDB_load_source(PARROT_INTERP, ARGIN(const char *command))
     PDB_file_t    *pfile;
     PDB_line_t    *pline;
     PDB_t         * const pdb = interp->pdb;
-    opcode_t      *pc         = pdb->cur_opcode;
+    opcode_t      *pc         = interp->code->base.data;
+
     unsigned long  size = 0;
+
+    TRACEDEB_MSG("PDB_load_source");
 
     /* If there was a file already loaded or the bytecode was
        disassembled, free it */
@@ -2382,6 +2394,9 @@ PDB_load_source(PARROT_INTERP, ARGIN(const char *command))
     pfile->source = (char *)mem_sys_allocate(1024);
     pfile->line   = pline;
     pline->number = 1;
+
+    PARROT_ASSERT(interp->op_info_table);
+    PARROT_ASSERT(pc);
 
     while ((c = fgetc(file)) != EOF) {
         /* Grow it */
@@ -2417,6 +2432,8 @@ PDB_load_source(PARROT_INTERP, ARGIN(const char *command))
 
     pdb->state |= PDB_SRC_LOADED;
     pdb->file   = pfile;
+
+    TRACEDEB_MSG("PDB_load_source finished");
 }
 
 /*
