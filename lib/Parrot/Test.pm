ashgti/parrot

commit 6924e7244de5289750ad11897ccc25b7484e2c61

Fix msvc warning in t/src/compiler.t:   LINK : warning LNK4044: Nicht erkannte Option "Lblib\lib"; ignoriert   And we don't want to use /LIBPATH:$PConfig{blib_dir} here, only for static.
git-svn-id: https://svn.parrot.org/parrot/trunk@34981 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

lib/Parrot/Test.pm
@@ -960,7 +960,9 @@ sub _generate_test_functions {
                 my $iculibs = $PConfig{has_icu} ? $PConfig{icu_shared} : q{};
                 my $libparrot =
                     $PConfig{parrot_is_shared}
-                    ? "$PConfig{rpath_blib} -L$PConfig{blib_dir} "
+                    ? ("$PConfig{rpath_blib} " .
+                      ($^O =~ m/MSWin32/ and $PConfig{cc} eq 'cl'
+                         ? "" : "-L$PConfig{blib_dir} "))
                     . (
                       $^O =~ m/MSWin32/
                     ? $PConfig{libparrot_ldflags}

commit 9c190ce3bf25d982a851057bac67a8b52091ee82

Fix a build error introduced in r34981 (precedence error).  This fixes TT #131.
git-svn-id: https://svn.parrot.org/parrot/trunk@35001 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

lib/Parrot/Test.pm
@@ -962,7 +962,7 @@ sub _generate_test_functions {
                 my $libparrot =
                     $PConfig{parrot_is_shared}
                     ? ("$PConfig{rpath_blib} " .
-                      ($^O =~ m/MSWin32/ and $PConfig{cc} eq 'cl'
+                      (($^O =~ m/MSWin32/ and $PConfig{cc} eq 'cl')
                          ? "" : "-L$PConfig{blib_dir} "))
                     . (
                       $^O =~ m/MSWin32/