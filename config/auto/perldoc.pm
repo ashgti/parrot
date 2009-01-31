ashgti/parrot

commit f8be4b99c465317f7807fc4f727f23f9b2c9c224

[configure] the generation of 'docs/Makefile' is not longer a special case
git-svn-id: https://svn.parrot.org/parrot/trunk@36115 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

config/auto/perldoc.pm
@@ -46,6 +46,48 @@ sub runstep {
 
     _handle_version($conf, $version, $cmd);
 
+    my $TEMP_pod_build = <<'E_NOTE';
+
+# the following part of the Makefile was built by 'config/auto/perldoc.pm'
+
+E_NOTE
+
+    opendir OPS, 'src/ops' or die "opendir ops: $!";
+    my @ops = sort grep { !/^\./ && /\.ops$/ } readdir OPS;
+    closedir OPS;
+
+    my $TEMP_pod = join q{ } =>
+        map { my $t = $_; $t =~ s/\.ops$/.pod/; "ops/$t" } @ops;
+
+    my $slash       = $conf->data->get('slash');
+    my $new_perldoc = $conf->data->get('new_perldoc');
+
+    foreach my $ops (@ops) {
+        my $pod = $ops;
+        $pod =~ s/\.ops$/.pod/;
+        if ( $new_perldoc ) {
+            $TEMP_pod_build .= <<"END"
+ops$slash$pod: ..${slash}src${slash}ops${slash}$ops
+\t\$(PERLDOC) -ud ops${slash}$pod ..${slash}src${slash}ops${slash}$ops
+\t\$(CHMOD) 0644 ops${slash}$pod
+
+END
+        }
+        else {
+            $TEMP_pod_build .= <<"END"
+ops$slash$pod: ..${slash}src${slash}ops${slash}$ops
+\t\$(PERLDOC) -u ..${slash}ops${slash}$ops > ops${slash}$pod
+\t\$(CHMOD) 0644 ..${slash}ops${slash}$pod
+
+END
+        }
+    }
+
+    $conf->data->set(
+        TEMP_pod             => $TEMP_pod,
+        TEMP_pod_build       => $TEMP_pod_build,
+    );
+
     return 1;
 }
 

config/gen/makefiles.pm
@@ -78,7 +78,6 @@ sub runstep {
     my ( $self, $conf ) = @_;
 
     $self->makefiles($conf);
-    $conf->append_configure_log('docs/Makefile');
     $self->cflags($conf);
 
     return 1;
@@ -123,55 +122,7 @@ sub makefiles {
         my $args   = $self->{makefiles}->{$target};
         my $source = delete $args->{SOURCE};
 
-        if ( $target ne 'docs/Makefile' ) {
-            $conf->genfile($source => $target, %$args );
-        }
-        else {
-
-            if ( $conf->data->get('has_perldoc') ) {
-
-                # set up docs/Makefile, partly based on the .ops in the root dir
-
-                opendir OPS, "src/ops" or die "opendir ops: $!";
-                my @ops = sort grep { !/^\./ && /\.ops$/ } readdir OPS;
-                closedir OPS;
-
-                my $pod = join " " =>
-                    map { my $t = $_; $t =~ s/\.ops$/.pod/; "ops/$t" } @ops;
-
-                $conf->data->set( pod => $pod );
-
-                $conf->genfile($source => $target, %$args );
-
-                $conf->data->set( pod => undef );
-
-                open my $MAKEFILE, ">>", "docs/Makefile"
-                    or die "open >> docs/Makefile: $!";
-
-                my $slash       = $conf->data->get('slash');
-                my $new_perldoc = $conf->data->get('new_perldoc');
-
-                foreach my $ops (@ops) {
-                    my $pod = $ops;
-                    $pod =~ s/\.ops$/.pod/;
-                    print {$MAKEFILE} "ops$slash$pod: ..${slash}src${slash}ops${slash}$ops\n";
-                    if ( $new_perldoc == 1 ) {
-                        print {$MAKEFILE} "\t\$(PERLDOC) -ud ops${slash}$pod"
-                            . " ..${slash}src${slash}ops${slash}$ops\n";
-                        print {$MAKEFILE} "\t\$(CHMOD) 0644 ops${slash}$pod\n\n";
-                    }
-                    else {
-                        print {$MAKEFILE} "\t\$(PERLDOC) -u ..${slash}ops${slash}$ops"
-                            . " > ops${slash}$pod\n";
-                        print {$MAKEFILE} "\t\$(CHMOD) 0644 ..${slash}ops${slash}$pod\n\n";
-                    }
-                }
-
-            }
-            else {
-                print "\nNo Perldoc, not generating a docs makefile.\n";
-            }
-        }
+        $conf->genfile($source => $target, %$args );
     }
     return;
 }

config/gen/makefiles/docs.in
@@ -9,7 +9,7 @@ CHMOD  = @chmod@
 PERLDOC = @perldoc@
 
 # long list of .pod files
-POD = @pod@
+POD = @TEMP_pod@
 
 #
 # Targets
@@ -35,7 +35,7 @@ doc-prep:
 	$(MKPATH) ops
 
 packfile-c.pod: ../src/packfile.c
-#IF(new_perldoc):	$(PERLDOC) -ud packfile-c.pod ../src/packfile.c 
+#IF(new_perldoc):	$(PERLDOC) -ud packfile-c.pod ../src/packfile.c
 #ELSE:	$(PERLDOC) -u ../src/packfile.c > packfile-c.pod
 
 clean:
@@ -47,3 +47,4 @@ html:
 html-clean:
 	$(PERL) -I../lib -MParrot::Distribution -e "Parrot::Distribution->new()->delete_html_docs()"
 
+@TEMP_pod_build@

commit 30e7ddb5bc7a555528883e090f630d21dd1ac6b3

[perldoc] fix RT #62920 (bug introduced by r36115)
git-svn-id: https://svn.parrot.org/parrot/trunk@36189 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

config/auto/perldoc.pm
@@ -98,6 +98,9 @@ sub _initial_content_check {
         $conf->data->set(
             has_perldoc => 0,
             new_perldoc => 0,
+            perldoc     => 'echo',
+            TEMP_pod        => '',
+            TEMP_pod_build  => '',
         );
         $self->set_result('no');
         return;

config/gen/makefiles/root.in
@@ -138,7 +138,7 @@ NONGEN_HEADERS   = @TEMP_nongen_headers@
 
 GEN_MAKEFILES = \
     Makefile \
-#IF(has_perldoc):    docs/Makefile \
+    docs/Makefile \
     compilers/json/Makefile \
     compilers/ncigen/Makefile \
     compilers/nqp/Makefile \

t/steps/auto_perldoc-01.t
@@ -68,7 +68,7 @@ is($conf->data->get('has_perldoc'), 0,
     "Got expected value for 'has_perldoc'");
 is($conf->data->get('new_perldoc'), 0,
     "Got expected value for 'new_perldoc'");
-is($conf->data->get('perldoc'), undef,
+is($conf->data->get('perldoc'), 'echo',
     "... and expected 'perldoc' path");
 
 $version = 1;