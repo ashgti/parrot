ashgti/parrot

commit d2b1d7d5304f74086b30a42379edf6b2b1669312

[debugger] use readline for interactive input (testing)
git-svn-id: https://svn.parrot.org/parrot/trunk@36427 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

src/debug.c
@@ -1195,6 +1195,13 @@ PDB_get_command(PARROT_INTERP)
     char         *c;
     PDB_t        * const pdb = interp->pdb;
 
+/***********************************
+   **** Testing ****
+   Do not delete yet
+   the commented out
+   parts
+***********************************/
+
     /* flush the buffered data */
     fflush(stdout);
 
@@ -1244,21 +1251,43 @@ PDB_get_command(PARROT_INTERP)
 
         c = pdb->cur_command;
 
-        Parrot_io_eprintf(pdb->debugger, "\n(pdb) ");
+        /*Parrot_io_eprintf(pdb->debugger, "\n(pdb) ");*/
+        Parrot_io_eprintf(pdb->debugger, "\n");
 
         /* skip leading whitespace */
+/*
         do {
             ch = fgetc(stdin);
         } while (isspace((unsigned char)ch) && ch != '\n');
+*/
+        {
+        Interp * pi = interp->debugger;
+        STRING * readline = CONST_STRING(pi, "readline_interactive");
+        STRING * prompt = CONST_STRING(pi, "(pdb) ");
+        STRING *s= Parrot_str_new(pi, NULL, 0);
+        PMC *stdin = Parrot_io_stdhandle(interp->debugger, 0, NULL);
+
+        Parrot_PCCINVOKE(interp->debugger,
+            stdin, readline,
+            "S->S", prompt, & s);
+        fprintf(stderr, "Hi\n");
+        {
+        char * aux = Parrot_str_to_cstring(interp->debugger, s);
+        strcpy(c, aux);
+        Parrot_str_free_cstring(aux);
+        }
+        ch = '\n';
+        }
 
         /* generate string (no more than buffer length) */
+/*
         while (ch != EOF && ch != '\n' && (i < DEBUG_CMD_BUFFER_LENGTH)) {
             c[i++] = (char)ch;
             ch     = fgetc(stdin);
         }
 
         c[i] = '\0';
-
+*/
         if (ch == -1)
             strcpy(c, "quit");
     }

commit 3fa6dfa1e7b840ed5700fbd7b12cf601a2dc103d

TT#286 - Fix bug introduced on darwin in r36427 - stdin is apparently a macro there, so using it as a variable name is not boss.
git-svn-id: https://svn.parrot.org/parrot/trunk@36429 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

src/debug.c
@@ -1265,10 +1265,10 @@ PDB_get_command(PARROT_INTERP)
         STRING * readline = CONST_STRING(pi, "readline_interactive");
         STRING * prompt = CONST_STRING(pi, "(pdb) ");
         STRING *s= Parrot_str_new(pi, NULL, 0);
-        PMC *stdin = Parrot_io_stdhandle(interp->debugger, 0, NULL);
+        PMC *tmp_stdin = Parrot_io_stdhandle(interp->debugger, 0, NULL);
 
         Parrot_PCCINVOKE(interp->debugger,
-            stdin, readline,
+            tmp_stdin, readline,
             "S->S", prompt, & s);
         {
         char * aux = Parrot_str_to_cstring(interp->debugger, s);
@@ -1282,7 +1282,7 @@ PDB_get_command(PARROT_INTERP)
 /*
         while (ch != EOF && ch != '\n' && (i < DEBUG_CMD_BUFFER_LENGTH)) {
             c[i++] = (char)ch;
-            ch     = fgetc(stdin);
+            ch     = fgetc(tmp_stdin);
         }
 
         c[i] = '\0';