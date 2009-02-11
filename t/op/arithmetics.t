ashgti/parrot

commit d48a6aeba581f5c6b3573cb63f8570fa3c5ae625

[t] Marked -0.0 tests as TODO instead of failing on MSWin32 and OpenBSD, per comments on TT #313.  This reverts and supersedes r36574.
git-svn-id: https://svn.parrot.org/parrot/trunk@36575 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

t/op/arithmetics.t
@@ -1,5 +1,5 @@
 #!perl
-# Copyright (C) 2001-2008, The Perl Foundation.
+# Copyright (C) 2001-2009, The Perl Foundation.
 # $Id$
 
 use strict;
@@ -169,22 +169,12 @@ OUTPUT
 # Operations on a single NUMVAL
 #
 
-my $output = <<OUTPUT;
--0
-0
--123.456789
-123.456789
--0
-0
--123.456789
-123.456789
-OUTPUT
+TODO: {
+    my @todo;
+    @todo = ( todo => '-0.0 not implemented, TT #313' )
+        if $^O =~ m/(?:openbsd|win32)/i;
 
-if ($^O =~ m/openbsd|win32/i) {
-    $output =~ s/-0$/0/mg;
-}
-
-pasm_output_is( <<'CODE', $output, "turn a native number into its negative" );
+pasm_output_is( <<'CODE', <<OUTPUT, 'negate a native number', @todo );
         set N0, 0
         neg N0
         print N0
@@ -220,6 +210,17 @@ pasm_output_is( <<'CODE', $output, "turn a native number into its negative" );
         print "\n"
         end
 CODE
+-0
+0
+-123.456789
+123.456789
+-0
+0
+-123.456789
+123.456789
+OUTPUT
+
+}
 
 pasm_output_is( <<'CODE', <<OUTPUT, "take the absolute of a native number" );
         set N0, 0

t/pmc/complex.t
@@ -1,5 +1,5 @@
 #! parrot
-# Copyright (C) 2001-2008, The Perl Foundation.
+# Copyright (C) 2001-2009, The Perl Foundation.
 # $Id$
 
 =head1 NAME
@@ -718,6 +718,28 @@ handler:
     is( $S3, $S1, $S4 )
 .endm
 
+.macro complex_op_todo( val, res, op )
+    $P1 = new ['Complex']
+    $P2 = new ['Complex']
+    set $P1, .val
+
+    set $S0, .val
+    set $S1, .res
+    set $S2, .op
+
+    #XXX: can't do $P1.'$S2'()
+    $P2 = $P1. $S2()
+    $S3 = sprintf "%f%+fi", $P2
+
+    concat $S4, $S2, " of "
+    concat $S4, $S4, $S0
+
+    $I0 = cmp_str $S1, $S3
+    $I0 = not $I0
+
+    todo( $I0, $S4 )
+.endm
+
 .sub ln_of_complex_numbers
     .complex_op_is("-2+0i", "0.693147+3.141593i", 'ln' )
     .complex_op_is("-1+0i", "0.000000+3.141593i", 'ln' )
@@ -1041,14 +1063,16 @@ handler:
     .complex_op_is("-2+3i", "3.590565+0.530921i", 'sinh' )
     .complex_op_is("-2-3i", "3.590565-0.530921i", 'sinh' )
 
-    eq osname, "openbsd", fail
-    eq osname, "MSWin32", fail
+    eq osname, "openbsd", todo
+    eq osname, "MSWin32", todo
     .complex_op_is("0-2i", "-0.000000-0.909297i", 'sinh' )
     .complex_op_is("0+2i", "-0.000000+0.909297i", 'sinh' )
     .return()
 
-fail:
-    skip( 2, 'sinh 0+2i, sinh 0-2i failing on your platform' )
+todo:
+    .complex_op_todo("0-2i", "-0.000000-0.909297i", 'sinh, TT #313' )
+    .complex_op_todo("0+2i", "-0.000000+0.909297i", 'sinh, TT #313' )
+    .return()
 .end
 
 .sub cosh_of_complex_numbers

t/pmc/float.t
@@ -1,5 +1,5 @@
 #!perl
-# Copyright (C) 2001-2006, The Perl Foundation.
+# Copyright (C) 2001-2009, The Perl Foundation.
 # $Id$
 
 use strict;
@@ -503,18 +503,21 @@ ok 1
 ok 2
 OUTPUT
 
-my $output = '-0';
-if ($^O =~ m/openbsd|win32/i) {
-    $output =~ s/-0$/0/mg;
-}
+TODO: {
+    my @todo;
+    @todo = ( todo => '-0.0 not implemented, TT #313' )
+        if $^O =~ m/(?:openbsd|win32)/i;
 
-pasm_output_is( << 'CODE', $output, "neg 0" );
+pasm_output_like( <<'CODE', <<'OUTPUT', 'neg 0', @todo );
     new P0, ['Float']
     set P0, 0.0
     neg P0
-        print P0
+    print P0
     end
 CODE
+/^-0/
+OUTPUT
+}
 
 pasm_output_is( << 'CODE', << 'OUTPUT', "Equality" );
     new P0, ['Float']

commit 326db9d8b21b4961248b3ceef75c93cd6c06a2e9

TT #313 win32 (mingw + msvc) - split tests into todo and working - fix complex todo error introduced by r36575 - clarify "inf is not platform-independent" for win32 msvc only, todo added
All win32 arithmetic tests should pass now



git-svn-id: https://svn.parrot.org/parrot/trunk@36579 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

t/op/arithmetics.t
@@ -7,7 +7,7 @@ use warnings;
 use lib qw( . lib ../lib ../../lib );
 
 use Test::More;
-use Parrot::Test tests => 28;
+use Parrot::Test tests => 29;
 
 # test for GMP
 use Parrot::Config;
@@ -166,7 +166,7 @@ CODE
 OUTPUT
 
 #
-# Operations on a single NUMVAL
+# print -0.0 as -0
 #
 
 TODO: {
@@ -174,7 +174,7 @@ TODO: {
     @todo = ( todo => '-0.0 not implemented, TT #313' )
         if $^O =~ m/(?:openbsd|win32)/i;
 
-pasm_output_is( <<'CODE', <<OUTPUT, 'negate a native number', @todo );
+pasm_output_is( <<'CODE', <<OUTPUT, 'negate -0.0', @todo );
         set N0, 0
         neg N0
         print N0
@@ -183,6 +183,25 @@ pasm_output_is( <<'CODE', <<OUTPUT, 'negate a native number', @todo );
         neg N0
         print N0
         print "\n"
+        set N0, -0.0
+        neg N1, N0
+        print N1
+        print "\n"
+CODE
+-0
+0
+-0
+0
+OUTPUT
+
+}
+
+
+#
+# Operations on a single NUMVAL
+#
+
+pasm_output_is( <<'CODE', <<OUTPUT, 'negate a native number' );
         set N0, 123.4567890
         neg N0
         print N0
@@ -196,10 +215,6 @@ pasm_output_is( <<'CODE', <<OUTPUT, 'negate a native number', @todo );
         neg N1, N0
         print N1
         print "\n"
-        set N0, -0.0
-        neg N1, N0
-        print N1
-        print "\n"
         set N0, 123.4567890
         neg N1, N0
         print N1
@@ -210,17 +225,12 @@ pasm_output_is( <<'CODE', <<OUTPUT, 'negate a native number', @todo );
         print "\n"
         end
 CODE
--0
-0
 -123.456789
 123.456789
--0
 0
 -123.456789
 123.456789
 OUTPUT
-
-}
 
 pasm_output_is( <<'CODE', <<OUTPUT, "take the absolute of a native number" );
         set N0, 0
@@ -913,8 +923,12 @@ CODE
 OUTPUT
 }
 
+TODO: {
+    my @todo;
+    @todo = ( todo => 'inf is not platform-independent' )
+        if $^O eq 'MSWin32' and $PConfig{cc} =~ /cl/i;
 
-pir_output_is( <<'CODE', <<OUTPUT, "Inf/NaN - basic arith" );
+pir_output_is( <<'CODE', <<OUTPUT, "Inf/NaN - basic arith", @todo );
 .sub 'test' :main
     $N0 = 'Inf'
     say $N0
@@ -926,6 +940,7 @@ Inf
 NaN
 OUTPUT
 
+}
 
 # Local Variables:
 #   mode: cperl

t/pmc/complex.t
@@ -718,7 +718,7 @@ handler:
     is( $S3, $S1, $S4 )
 .endm
 
-.macro complex_op_todo( val, res, op )
+.macro complex_op_todo( val, res, op, todo )
     $P1 = new ['Complex']
     $P2 = new ['Complex']
     set $P1, .val
@@ -726,13 +726,14 @@ handler:
     set $S0, .val
     set $S1, .res
     set $S2, .op
+    set $S3, .todo
 
     #XXX: can't do $P1.'$S2'()
     $P2 = $P1. $S2()
     $S3 = sprintf "%f%+fi", $P2
 
-    concat $S4, $S2, " of "
-    concat $S4, $S4, $S0
+    concat $S5, $S2, " of "
+    concat $S5, $S5, $S4
 
     $I0 = cmp_str $S1, $S3
     $I0 = not $I0
@@ -1070,8 +1071,8 @@ handler:
     .return()
 
 todo:
-    .complex_op_todo("0-2i", "-0.000000-0.909297i", 'sinh, TT #313' )
-    .complex_op_todo("0+2i", "-0.000000+0.909297i", 'sinh, TT #313' )
+    .complex_op_todo("0-2i", "-0.000000-0.909297i", 'sinh', 'TT #313' )
+    .complex_op_todo("0+2i", "-0.000000+0.909297i", 'sinh', 'TT #313' )
     .return()
 .end
 