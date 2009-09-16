efb8ce5f23d056bfe1f8c1814270d6115986f48e
darbelo <darbelo@d31e2699-5ff4-0310-a27c-f18f2fbe73fe>
2009-09-16 17:44:18 +0000

    Add a new configure step to determine a platform's capability to build JIT call frames. Run but not used yet.
    git-svn-id: https://svn.parrot.org/parrot/branches/kill_jit@41300 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

---
+++ config/auto/frames.pm
@@ -0,0 +1,57 @@
+# Copyright (C) 2009, Parrot Foundation.
+# $Id$
+
+=head1 NAME
+
+config/auto/frmes - 
+
+=head1 DESCRIPTION
+
+Determines whether the current platform is capable of building NCI call
+frames dynamically.  Use the C<--buildframes> option to override the
+default value for your CPU architecture and operating system.
+
+=cut
+
+package auto::frames;
+
+use strict;
+use warnings;
+
+use base qw(Parrot::Configure::Step);
+
+sub _init {
+    my $self = shift;
+    my %data;
+    $data{description} = q{Determine call frame building capability};
+    $data{result}      = q{};
+    return \%data;
+}
+
+sub runstep {
+    my ( $self, $conf ) = @_;
+
+    my $can_build_call_frames = 0;
+
+    my $osname  = $conf->data->get('osname');
+    my $cpuarch = $conf->data->get('cpuarch');
+    my $nvsize  = $conf->data->get('nvsize');
+
+    if (defined $conf->options->get('buildframes') || ($nvsize == 8
+        && $cpuarch eq 'i386' && $osname ne 'darwin')) {
+        $can_build_call_frames = 1;
+    }
+
+    $conf->data->set( can_build_call_frames => $can_build_call_frames );
+    $self->set_result($can_build_call_frames?'yes':'no');
+    return 1;
+}
+
+1;
+
+# Local Variables:
+#   mode: cperl
+#   cperl-indent-level: 4
+#   fill-column: 100
+# End:
+# vim: expandtab shiftwidth=4:
