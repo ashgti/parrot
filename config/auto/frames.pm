853898ad79af6c7ab3a65044a05b431fe9e2c35b
darbelo <darbelo@d31e2699-5ff4-0310-a27c-f18f2fbe73fe>
2009-09-18 20:48:55 +0000

    Recycle the old auto::jit exec_protect probes for auto::frames. Shuffle some files around.
    git-svn-id: https://svn.parrot.org/parrot/branches/kill_jit@41338 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- config/auto/frames.pm
+++ config/auto/frames.pm
@@ -40,11 +40,9 @@ sub runstep {
     if (defined $conf->options->get('buildframes')) {
         $can_build_call_frames = $conf->options->get('buildframes');
     }
-    elsif ($nvsize == 8 && $cpuarch eq 'i386' && $osname ne 'darwin') {
-        $can_build_call_frames = 1;
-    }
     else {
-        $can_build_call_frames = 0;
+        $can_build_call_frames = ($nvsize == 8 && $cpuarch eq 'i386'
+            && $osname ne 'darwin');
     }
 
     if ( $can_build_call_frames ) {
@@ -58,9 +56,39 @@ sub runstep {
         );
     }
 
+    if ( $can_build_call_frames ) {
+        # test for executable malloced memory
+        if ( -e "config/auto/frames/test_exec_${osname}_c.in" ) {
+            $conf->cc_gen("config/auto/frames/test_exec_${osname}_c.in");
+            eval { $conf->cc_build(); };
+            if ($@) {
+                $conf->data->set( has_exec_protect => 0 );
+            }
+            else {
+                my $exec_protect_test = (
+                    $conf->cc_run(0) !~ /ok/ && $conf->cc_run(1) =~ /ok/
+                );
+                _handle_exec_protect($conf, $exec_protect_test);
+            }
+            $conf->cc_clean();
+        }
+        else {
+            $conf->data->set( has_exec_protect => 0 );
+        }
+    }
 
     $self->set_result($can_build_call_frames?'yes':'no');
     return 1;
+}
+
+sub _handle_exec_protect {
+    my ($conf, $exec_protect_test) = @_;
+    if ($exec_protect_test) {
+        $conf->data->set( has_exec_protect => 1 );
+    }
+    else {
+        $conf->data->set( has_exec_protect => 0 );
+    }
 }
 
 1;
