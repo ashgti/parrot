a1f79412240d754c38f7270ca76cc80e5dbbed0e
bacek <bacek@d31e2699-5ff4-0310-a27c-f18f2fbe73fe>
2009-10-01 09:28:19 +0000

    Initialise Context.caller_ctx and current_sig in Parrot_init_context
    git-svn-id: https://svn.parrot.org/parrot/branches/pcc_reapply@41588 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- src/call/context.c
+++ src/call/context.c
@@ -1197,6 +1197,8 @@ init_context(PARROT_INTERP, ARGMOD(PMC *pmcctx), ARGIN_NULLOK(PMC *pmcold))
     ctx->handlers          = PMCNULL;
     ctx->caller_ctx        = NULL;
     ctx->pred_offset       = 0;
+    ctx->caller_ctx        = old;
+    ctx->current_sig       = NULL;
 
     if (old) {
         /* some items should better be COW copied */
