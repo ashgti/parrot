b7e8504334e5f292b7dd33bf2a28aad595dd650b
mikehh <mikehh@d31e2699-5ff4-0310-a27c-f18f2fbe73fe>
2009-12-30 19:48:13 +0000

    fix codetest failure - there should be one space or a newline after a comma and unwrapped macro arguments
    git-svn-id: https://svn.parrot.org/parrot/branches/boehm_gc@43326 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- src/gc/alloc_memory.c
+++ src/gc/alloc_memory.c
@@ -29,7 +29,7 @@ setup function to initialize the memory pools.
 #  define malloc GC_MALLOC_UNCOLLECTABLE
 #  define free   GC_FREE
 #  define realloc GC_REALLOC
-#  define calloc(s,n) GC_MALLOC_UNCOLLECTABLE(s*n)
+#  define calloc(s, n) GC_MALLOC_UNCOLLECTABLE((s)*(n))
 #endif
 
 PARROT_DOES_NOT_RETURN
