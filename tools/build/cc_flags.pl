6f6388355876ede7a70ac8c99548d369b171a067
darbelo <darbelo@d31e2699-5ff4-0310-a27c-f18f2fbe73fe>
2010-02-01 06:07:05 +0000

    Sync the branch with trunk.
    git-svn-id: https://svn.parrot.org/parrot/branches/pmc_freeze_with_pmcs@43680 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- tools/build/cc_flags.pl
+++ tools/build/cc_flags.pl
@@ -6,11 +6,11 @@
 
 =head1 NAME
 
-tools/dev/cc_flags.pl - Process compiler flags
+tools/build/cc_flags.pl - Process compiler flags
 
 =head1 SYNOPSIS
 
-    % perl tools/dev/cc_flags.pl transform compiler flags
+    % perl tools/build/cc_flags.pl transform compiler flags
 
 =head1 DESCRIPTION
 
@@ -29,13 +29,6 @@ F<config/gen/cflags/root.in>.
 
 use strict;
 use warnings;
-
-my $verbose;
-
-if ($ARGV[0] eq '-v') {
-    $verbose = 1;
-    shift;
-}
 
 my $cflags = shift;
 
@@ -118,8 +111,6 @@ if ($cfile) {
         }
     }
 
-    # print "@ARGV\n";
-
     # Visual C++ already prints the source file name...
     if ( $ARGV[0] =~ /cl(?:\.exe)?/i ) {
 
@@ -132,10 +123,6 @@ if ($cfile) {
     else {
         print "$cfile\n";
     }
-}
-
-if ($verbose) {
-    print join ' ', @ARGV;
 }
 
 exit system(@ARGV) / 256;
