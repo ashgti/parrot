8fe6ea68f9e30f7d448130af087e26f82ed6b82c
mikehh <mikehh@d31e2699-5ff4-0310-a27c-f18f2fbe73fe>
2010-02-09 21:46:33 +0000

    fix codetest failure - at least one space between C keyword and subsequent open parenthesis
    git-svn-id: https://svn.parrot.org/parrot/branches/gc_encapsulate_part2@43831 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- src/gc/gc_private.h
+++ src/gc/gc_private.h
@@ -115,10 +115,10 @@ typedef struct GC_Subsystem {
     void (*free_pmc_header)(PARROT_INTERP, PMC *);
 
     STRING* (*allocate_string_header)(PARROT_INTERP, UINTVAL flags);
-    void    (*free_string_header)(PARROT_INTERP, STRING*);
+    void (*free_string_header)(PARROT_INTERP, STRING*);
 
     Buffer* (*allocate_bufferlike_header)(PARROT_INTERP, size_t size);
-    void    (*free_bufferlike_header)(PARROT_INTERP, Buffer*, size_t size);
+    void (*free_bufferlike_header)(PARROT_INTERP, Buffer*, size_t size);
 
     void* (*allocate_pmc_attributes)(PARROT_INTERP, PMC *);
     void (*free_pmc_attributes)(PARROT_INTERP, PMC *);
