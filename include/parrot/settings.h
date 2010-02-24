6458f27318d37af3c4383a250a31593c0f846ce6
bacek <bacek@d31e2699-5ff4-0310-a27c-f18f2fbe73fe>
2010-02-24 20:21:45 +0000

    Switch default GC to MS.
    git-svn-id: https://svn.parrot.org/parrot/branches/boehm_gc_2@44460 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- include/parrot/settings.h
+++ include/parrot/settings.h
@@ -37,12 +37,9 @@
  * GC_DEFAULT_TYPE selection
  * MS  -- stop-the-world mark & sweep
  * INF -- infinite memory "collector"
+ * BOEHM -- Boehm GC
  */
-#ifdef PARROT_HAS_BOEHM_GC
-#  define PARROT_GC_DEFAULT_TYPE BOEHM
-#else
-#  define PARROT_GC_DEFAULT_TYPE MS
-#endif
+#define PARROT_GC_DEFAULT_TYPE MS
 
 /*
  * JIT/i386 can use the CGP run core for external functions instead
