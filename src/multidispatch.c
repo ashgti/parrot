5d137dadef4f04118ba0a54697c656b9721b1812
allison <allison@d31e2699-5ff4-0310-a27c-f18f2fbe73fe>
2010-03-07 00:25:07 +0000

    [pcc] Updated more invocations from C to fetch return values after invoke.
    git-svn-id: https://svn.parrot.org/parrot/branches/pcc_hackathon_6Mar10@44720 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- src/multidispatch.c
+++ src/multidispatch.c
@@ -286,24 +286,25 @@ Parrot_mmd_multi_dispatch_from_c_args(PARROT_INTERP,
         ARGIN(const char *name), ARGIN(const char *sig), ...)
 {
     ASSERT_ARGS(Parrot_mmd_multi_dispatch_from_c_args)
-    PMC *sig_object, *sub;
-
+    PMC *call_obj, *sub;
     va_list args;
+    char *arg_sig, *ret_sig;
+    Parrot_pcc_split_signature_string(interp, sig, &arg_sig, &ret_sig);
+
     va_start(args, sig);
-    sig_object = Parrot_pcc_build_sig_object_from_varargs(interp, PMCNULL, sig, args);
-    va_end(args);
+    call_obj = Parrot_pcc_build_call_from_varargs(interp, PMCNULL, arg_sig, &args);
 
     /* Check the cache. */
     sub = Parrot_mmd_cache_lookup_by_types(interp, interp->op_mmd_cache, name,
-            VTABLE_get_pmc(interp, sig_object));
+            VTABLE_get_pmc(interp, call_obj));
 
     if (PMC_IS_NULL(sub)) {
         sub = Parrot_mmd_find_multi_from_sig_obj(interp,
-            Parrot_str_new_constant(interp, name), sig_object);
+            Parrot_str_new_constant(interp, name), call_obj);
 
         if (!PMC_IS_NULL(sub))
             Parrot_mmd_cache_store_by_types(interp, interp->op_mmd_cache, name,
-                    VTABLE_get_pmc(interp, sig_object), sub);
+                    VTABLE_get_pmc(interp, call_obj), sub);
     }
 
     if (PMC_IS_NULL(sub))
@@ -318,7 +319,9 @@ Parrot_mmd_multi_dispatch_from_c_args(PARROT_INTERP,
             VTABLE_name(interp, sub));
 #endif
 
-    Parrot_pcc_invoke_from_sig_object(interp, sub, sig_object);
+    Parrot_pcc_invoke_from_sig_object(interp, sub, call_obj);
+    Parrot_pcc_fill_params_from_varargs(interp, call_obj, ret_sig, &args);
+    va_end(args);
 }
 
 
