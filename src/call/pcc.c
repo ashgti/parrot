5c1fced00b56f3aafb2394b92ba659fb8b3a6903
bacek <bacek@d31e2699-5ff4-0310-a27c-f18f2fbe73fe>
2010-03-16 07:31:33 +0000

    Save/restore old call_object in invoke_sub.
    git-svn-id: https://svn.parrot.org/parrot/branches/pcc_hackathon_6Mar10@44952 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- src/call/pcc.c
+++ src/call/pcc.c
@@ -77,7 +77,9 @@ Parrot_pcc_invoke_sub_from_c_args(PARROT_INTERP, ARGIN(PMC *sub_obj),
     ASSERT_ARGS(Parrot_pcc_invoke_sub_from_c_args)
     PMC    *call_obj;
     va_list args;
-    char *arg_sig, *ret_sig;
+    char   *arg_sig, *ret_sig;
+    PMC    *old_call_obj = Parrot_pcc_get_signature(interp, CURRENT_CONTEXT(interp));
+
     Parrot_pcc_split_signature_string(interp, sig, &arg_sig, &ret_sig);
 
     va_start(args, sig);
@@ -89,7 +91,7 @@ Parrot_pcc_invoke_sub_from_c_args(PARROT_INTERP, ARGIN(PMC *sub_obj),
     Parrot_pcc_fill_params_from_varargs(interp, call_obj, ret_sig, &args,
             PARROT_ERRORS_RESULT_COUNT_FLAG);
     va_end(args);
-
+    Parrot_pcc_set_signature(interp, CURRENT_CONTEXT(interp), old_call_obj);
 }
 
 
@@ -226,6 +228,7 @@ Parrot_pcc_invoke_method_from_c_args(PARROT_INTERP, ARGIN(PMC* pmc),
     PMC    *sub_obj;
     va_list args;
     char *arg_sig, *ret_sig;
+    PMC    *old_call_obj = Parrot_pcc_get_signature(interp, CURRENT_CONTEXT(interp));
     Parrot_pcc_split_signature_string(interp, signature, &arg_sig, &ret_sig);
 
     va_start(args, signature);
@@ -247,6 +250,7 @@ Parrot_pcc_invoke_method_from_c_args(PARROT_INTERP, ARGIN(PMC* pmc),
     Parrot_pcc_fill_params_from_varargs(interp, call_obj, ret_sig, &args,
             PARROT_ERRORS_RESULT_COUNT_FLAG);
     va_end(args);
+    Parrot_pcc_set_signature(interp, CURRENT_CONTEXT(interp), old_call_obj);
 }
 
 
