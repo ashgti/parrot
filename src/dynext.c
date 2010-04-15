9cc69aaedad3fd463cb4525647ea2c58da45c3f4
bacek <bacek@d31e2699-5ff4-0310-a27c-f18f2fbe73fe>
2010-04-15 14:40:54 +0000

    Remove last references to Parrot_str_append
    git-svn-id: https://svn.parrot.org/parrot/branches/immutable_strings_part1@45698 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- src/dynext.c
+++ src/dynext.c
@@ -350,7 +350,7 @@ get_path(PARROT_INTERP, ARGMOD_NULLOK(STRING *lib), Parrot_dlopen_flags flags,
     /* And on cygwin replace a leading "lib" by "cyg". */
 #ifdef __CYGWIN__
     if (!STRING_IS_EMPTY(lib) && memcmp(lib->strstart, "lib", 3) == 0) {
-        path = Parrot_str_append(interp, CONST_STRING(interp, "cyg"),
+        path = Parrot_str_concat(interp, CONST_STRING(interp, "cyg"),
             Parrot_str_substr(interp, lib, 3, lib->strlen - 3, NULL, 0));
 
         *handle = dlopen_string(interp, flags, path);
