f24584c80c9c3af6ef96b9440318e5b6c904c32b
mikehh <mikehh@d31e2699-5ff4-0310-a27c-f18f2fbe73fe>
2010-04-17 18:34:39 +0000

    fix a const to get g++ to build
    git-svn-id: https://svn.parrot.org/parrot/branches/immutable_strings_part1@45764 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- src/string/encoding/ucs2.c
+++ src/string/encoding/ucs2.c
@@ -459,7 +459,7 @@ ucs2_encode_and_advance(PARROT_INTERP, ARGMOD(String_iter *i), UINTVAL c)
     ASSERT_ARGS(ucs2_encode_and_advance)
 
 #if PARROT_HAS_ICU
-    UChar    *s = (const UChar*) i->str->strstart;
+    UChar    *s = (UChar*) i->str->strstart;
     UINTVAL pos = i->bytepos / sizeof (UChar);
     s[pos++]    = (UChar)c;
     i->charpos++;
